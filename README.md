2c84ed6c01a32e233dfca64971050e07599d44c2
Ugo <ugo.briasco@sumup.com>
2020-12-07 17:28:05 +0100

    Intial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# generator-basic-rest [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url] [![Coverage percentage][coveralls-image]][coveralls-url]
+> A scaffold for REST API
+
+## Installation
+
+First, install [Yeoman](http://yeoman.io) and generator-basic-rest using [npm](https://www.npmjs.com/) (we assume you have pre-installed [node.js](https://nodejs.org/)).
+
+```bash
+npm install -g yo
+npm install -g generator-basic-rest
+```
+
+Then generate your new project:
+
+```bash
+yo basic-rest
+```
+
+## Getting To Know Yeoman
+
+ * Yeoman has a heart of gold.
+ * Yeoman is a person with feelings and opinions, but is very easy to work with.
+ * Yeoman can be too opinionated at times but is easily convinced not to be.
+ * Feel free to [learn more about Yeoman](http://yeoman.io/).
+
+## License
+
+MIT © [Ugo Briasco](https://ugobriasco.me)
+
+
+[npm-image]: https://badge.fury.io/js/generator-basic-rest.svg
+[npm-url]: https://npmjs.org/package/generator-basic-rest
+[travis-image]: https://travis-ci.com/ugobriasco/generator-basic-rest.svg?branch=master
+[travis-url]: https://travis-ci.com/ugobriasco/generator-basic-rest
+[daviddm-image]: https://david-dm.org/ugobriasco/generator-basic-rest.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/ugobriasco/generator-basic-rest
+[coveralls-image]: https://coveralls.io/repos/ugobriasco/generator-basic-rest/badge.svg
+[coveralls-url]: https://coveralls.io/r/ugobriasco/generator-basic-rest
---
+++ app/index.js
@@ -0,0 +1,84 @@
+"use strict";
+// https://github.com/cdimascio/generator-express-no-stress/blob/master/app/index.js
+const Generator = require("yeoman-generator");
+
+module.exports = class extends Generator {
+  constructor(args, opts) {
+    super(args, opts);
+    this.argument("appname", { type: String, required: false });
+
+    this.name = this.options.appname || "_myapp";
+    this.description = "My cool app";
+    this.version = "1.0.0";
+  }
+
+  initializing() {}
+
+  async prompting() {
+    const prompts = [
+      {
+        type: "input",
+        name: "description",
+        message: `App description [${this.description}]`
+      },
+      {
+        type: "input",
+        name: "apiVersion",
+        message: `Version [${this.version}]`
+      }
+    ];
+
+    if (!this.options.appname) {
+      prompts.unshift({
+        type: "input",
+        name: "name",
+        message: `App name [${this.name}]`
+      });
+    }
+
+    await this.prompt(prompts).then(r => {
+      this.name = r.name ? r.name : this.name;
+      this.description = r.description ? r.description : this.description;
+      this.version = r.version ? r.version : this.version;
+    });
+  }
+
+  configuring() {}
+
+  default() {}
+
+  get writing() {
+    return {
+      appStaticFiles() {
+        console.log("name");
+        console.log(this.name);
+        console.log(this.sourceRoot());
+
+        const src = this.sourceRoot();
+        const dest = this.destinationPath(this.name);
+        const files = ["package.json"];
+        const opts = {
+          name: this.name,
+          description: this.description,
+          version: this.version
+        };
+
+        this.fs.copy(src, dest);
+
+        files.forEach(f => {
+          this.fs.copyTpl(
+            this.templatePath(f),
+            this.destinationPath(`${this.name}/${f}`),
+            opts
+          );
+        });
+      }
+    };
+  }
+
+  conflicts() {}
+
+  install() {}
+
+  end() {}
+};
