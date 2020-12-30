90839ecdcacde0cf7a611d7ed4b7eccf5973e519
Ugo <ugo.briasco@sumup.com>
2020-12-30 16:21:13 +0100

    Add Open Weather API adapter

---
+++ generators/app/templates/api-lib/open-weather/build-exclusion-params.js
@@ -0,0 +1,20 @@
+const parseCommaSeparatedString = (buffer, fragment) =>
+  buffer === '' ? fragment : `,${fragment}`;
+
+const buildExclusionParams = arr => {
+  if (!arr) return undefined;
+  const paramsEnabled = ['current', 'minutely', 'hourly', 'daily', 'alerts'];
+
+  const selection = arr.filter(param => paramsEnabled.includes(param));
+
+  if (selection.length > 0) {
+    return selection.reduce((buffer, param) => {
+      const fragment = parseCommaSeparatedString(buffer, param);
+      return buffer + fragment;
+    });
+  } else {
+    return undefined;
+  }
+};
+
+module.exports = buildExclusionParams;
