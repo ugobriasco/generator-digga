0381e37b6ec19136cb62eccca4a22238dc90eba1
Ugo <ugo.briasco@sumup.com>
2020-12-30 12:38:41 +0100

    Fix tests

--- __tests__/app.js
+++ __tests__/app.js
@@ -5,13 +5,26 @@ const path = require('path');
 const assert = require('yeoman-assert');
 
 describe('generator-digga:app', () => {
-  beforeAll(() => {
+  jest.setTimeout(30000);
+
+  it('scaffolds a full project', () => {
+    const answers = {
+      name: 'myApp',
+      description: 'a test app',
+      version: '0.0.0',
+      database: 'none'
+    };
     return helpers
       .run(path.join(__dirname, '../generators/app'))
-      .withPrompts({ someAnswer: true });
-  });
-
-  it('say yes', () => {
-    assert(true);
+      .withPrompts(answers)
+      .then(() => {
+        console.log(path.join(__dirname, '../generators/app'));
+        assert.file([
+          'README.MD',
+          'index.js',
+          'package.json',
+          'prettier.config.js'
+        ]);
+      });
   });
 });
