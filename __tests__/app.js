3727df42c3ca1d3ef3c4edebf4c45f029dd00808
Ugo <ugo.briasco@sumup.com>
2021-01-03 12:23:19 +0100

    Update dependancies to latest

--- __tests__/app.js
+++ __tests__/app.js
@@ -5,26 +5,36 @@ const path = require('path');
 const assert = require('yeoman-assert');
 
 describe('generator-digga:app', () => {
-  jest.setTimeout(40000);
+  jest.setTimeout(60000);
 
-  it('scaffolds a full project', () => {
-    const answers = {
-      name: 'myApp',
-      description: 'a test app',
-      version: '0.0.0',
-      database: 'none'
-    };
-    return helpers
+  describe("Minimal installation", () => {
+    beforeAll(() => {
+      const answers = {
+        name: 'myApp',
+        description: 'a test app',
+        version: '0.0.0',
+        database: 'none'
+      };
+      return helpers
       .run(path.join(__dirname, '../generators/app'))
       .withPrompts(answers)
-      .then(() => {
-        console.log(path.join(__dirname, '../generators/app'));
-        assert.file([
-          'README.MD',
-          'index.js',
-          'package.json',
-          'prettier.config.js'
-        ]);
-      });
-  });
+    })
+    it('generates a scaffold application', () => {
+      assert.file([
+            'README.MD',
+            'index.js',
+            'package.json',
+            'prettier.config.js',
+            '.travis.yml',
+            '.gitignore',
+            'lib/index.js',
+            'lib/routes/index.js',
+            'lib/helpers/index.js',
+            'lib/config/index.js'
+          ]);
+    });
+    it('does not include the api module', () => {
+      assert.noFile(['lib/api/index.js'])
+    })
+  });   
 });
