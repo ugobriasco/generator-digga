0ba4f12108f6b05ebe193220783d9d0409592a54
Ugo <ugo.briasco@sumup.com>
2021-01-03 18:16:48 +0100

    Include argument to skip npm install

--- __tests__/app.js
+++ __tests__/app.js
@@ -3,14 +3,14 @@
 const helpers = require('yeoman-test');
 const path = require('path');
 const assert = require('yeoman-assert');
+const fs = require('fs');
 
 describe('generator-digga:app', () => {
-  jest.setTimeout(60000);
-
-  describe("Minimal installation", () => {
+ 
+  describe("Genereate minimal app", () => {
     beforeAll(() => {
       const answers = {
-        name: 'myApp',
+        name: 'minimal-app',
         description: 'a test app',
         version: '0.0.0',
         database: 'none'
@@ -18,6 +18,7 @@ describe('generator-digga:app', () => {
       return helpers
       .run(path.join(__dirname, '../generators/app'))
       .withPrompts(answers)
+      .withArguments(['noInstall']) 
     })
     it('generates a scaffold application', () => {
       assert.file([
@@ -36,5 +37,87 @@ describe('generator-digga:app', () => {
     it('does not include the api module', () => {
       assert.noFile(['lib/api/index.js'])
     })
-  });   
+  });
+
+
+  describe("Generate app with api modules", () => {
+    beforeAll(() => {
+      const answers = {
+        name: 'full-api-app',
+        description: 'a test app',
+        version: '0.0.0',
+        database: 'none',
+        api: ['openWeather','auth0']
+      };
+      return helpers
+      .run(path.join(__dirname, '../generators/app'))
+      .withPrompts(answers)
+      .withArguments(['noInstall']) 
+    })
+    it('generates a scaffold application, including api module', () => {
+      assert.file([
+            'README.MD',
+            'index.js',
+            'package.json',
+            'prettier.config.js',
+            '.travis.yml',
+            '.gitignore',
+            'lib/index.js',
+            'lib/routes/index.js',
+            'lib/helpers/index.js',
+            'lib/config/index.js',
+            'lib/config/api.config.js',
+            'lib/api/index.js'
+          ]);
+    });
+  });
+
+  describe("Generate app with mongoDB", () => {
+    beforeAll(() => {
+      const answers = {
+        name: 'full-api-app',
+        description: 'a test app',
+        version: '0.0.0',
+        database: 'mongoDB'
+      };
+      return helpers
+      .run(path.join(__dirname, '../generators/app'))
+      .withPrompts(answers)
+      .withArguments(['noInstall']) 
+    })
+    it('generates a scaffold application', () => {
+      assert.file([
+        'README.MD',
+        'index.js',
+        'package.json',
+        'prettier.config.js',
+        '.travis.yml',
+        '.gitignore',
+        'lib/index.js',
+        'lib/routes/index.js',
+        'lib/helpers/index.js',
+        'lib/config/index.js'
+      ]);
+    });
+  });
+
+
+  describe("Installs dependencies after generation", () => {
+    jest.setTimeout(60000);
+    beforeAll(() => {
+      const answers = {
+        name: 'app',
+        description: 'a test app',
+        version: '0.0.0',
+        database: 'none'
+      };
+      return helpers
+      .run(path.join(__dirname, '../generators/app'))
+      .withPrompts(answers)
+    })
+    it('injects dependendies', () => {
+      assert.noFileContent('package.json', 'dependencies: {}');
+      assert.noFileContent('package.json', 'devDependencies: {}');
+    });
+  });  
 });
