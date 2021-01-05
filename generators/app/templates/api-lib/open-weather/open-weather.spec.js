0a57b9302de78dbae6760098f4434212781d900b
Ugo <ugo.briasco@sumup.com>
2021-01-05 22:40:15 +0100

    Increase coverage of api/openWeather

--- generators/app/templates/api-lib/open-weather/open-weather.spec.js
+++ generators/app/templates/api-lib/open-weather/open-weather.spec.js
@@ -1,18 +1,34 @@
-const buildExclusionParams = require('./build-exclusion-params');
+const { buildExclusionParams, parseCommaSeparatedString } = require('./build-exclusion-params');
 
 describe('API::OpenWeather', () => {
-  it('Build a csv of exlusion parameters', () => {
-    const arr = ['current', 'hourly', 'bananas'];
-    const paramStr = buildExclusionParams(arr);
-    expect(paramStr).toBe('current,hourly');
+
+  describe('API::OpenWeather::buildExclusionParams', () => {
+    it('Build a csv of exlusion parameters', () => {
+      const arr = ['current', 'hourly', 'bananas'];
+      const paramStr = buildExclusionParams(arr);
+      expect(paramStr).toBe('current,hourly');
+    });
+    it('Returns undefined if no valid parameter presented', () => {
+      const arr = ['kiwis', 'apples', 'bananas'];
+      const paramStr = buildExclusionParams(arr);
+      expect(paramStr).toBe(undefined);
+    });
+    it('Returns an undefined if no array is presented', () => {
+      const paramStr = buildExclusionParams();
+      expect(paramStr).toBe(undefined);
+    });
+    describe('API::OpenWeather::buildExclusionParams::parseCommaSeparatedString', () => { 
+      it('it parse a comma if the bufferStr is not empty', () => {
+        const str = parseCommaSeparatedString('hello', 'there');
+        expect(str).toBe(',there');
+      })
+      it('it parse no comma if the bufferStr is empty', () => {
+        const str = parseCommaSeparatedString('', 'hello');
+        expect(str).toBe('hello');
+      })
+    })
   });
-  it('Returns undefined if no valid parameter presented', () => {
-    const arr = ['kiwis', 'apples', 'bananas'];
-    const paramStr = buildExclusionParams(arr);
-    expect(paramStr).toBe(undefined);
-  });
-  it('Returns an undefined if no array is presented', () => {
-    const paramStr = buildExclusionParams();
-    expect(paramStr).toBe(undefined);
-  });
+
+  
+  
 });
